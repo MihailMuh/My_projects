8467036ee9f81e25fbc6dd7f79aeb19e41d7ef90
MihailMuh <mukhortovm2004@mail>
2020-06-18 18:57:00 +0500

    Исправление ошибки с настройками

--- star_wars-the_joke-game/scriptspy/system_size.py
+++ star_wars-the_joke-game/scriptspy/system_size.py
@@ -1,5 +1,16 @@
 from tkinter import *
 
-window = Tk()
-x = window.winfo_screenwidth()
-y = window.winfo_screenheight() + 50
+x = 1370
+y = 800
+
+
+def windows():
+    global x, y
+    window = Tk()
+    x = window.winfo_screenwidth()
+    y = window.winfo_screenheight() + 50
+    window.destroy()
+    return x, y
+
+
+windows()
